6980b49a624005a8d92418a8ec946fbb2afee475
Jason Smith <jhs@couchone.com>
2010-12-20 22:14:57 +0700

    A somewhat useful test

--- send.js
+++ send.js
@@ -1,13 +1,24 @@
 var term_to_binary = require('./term_to_binary');
 
 //require('sys').puts(exports.term_to_binary(5));
-var t = ["jason", 'hunter', 'smith'];
-t = [1, 2, 3];
+var t = [                          // array
+        , ['two', 'array']         // Innocent 2-array
+        , "ABCDE"                  // string (char list, encoded as STRING)
+        , [/a/, 'foo']             // atom
+        , [/a/, 'GET']
+        , [/b/, 'ภาษาไทย']         // binary
+        , [[[[23, 'skidoo']]]]     // nested objects
+        , [/t/, [ 'tuple'          // tuple
+                , 'here'
+                , [/a/, 'too']
+                ]
+          ]
+        ];
 
 console.log("Sending: " + require('sys').inspect(t));
 var b = term_to_binary.term_to_binary(t);
 var fs = require('fs');
-fs.open('/tmp/out', 'w', 066, function(er, fd) {
+fs.open('/tmp/out', 'w', 0644, function(er, fd) {
   if(er) throw er;
   fs.write(fd, b, 0, b.length, null, function(er, bytes) {
     if(er) throw er;
